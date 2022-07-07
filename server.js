3f4a596f4916e2eda93cece2c45095d41220e820
Chad Smith <chad.smith@vanderbilt.edu>
2022-07-06 23:32:34 -0400

    connected to database

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// LAST ROUTE 
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// Start server on port 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
