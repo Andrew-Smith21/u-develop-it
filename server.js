621477b8b12ed4b65c88f0e354e10f672e5a96cb
Chad Smith <chad.smith@vanderbilt.edu>
2022-07-06 23:44:23 -0400

    create database calls

--- server.js
+++ server.js
@@ -22,6 +22,40 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
+
+// Return all data in candidates table
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//    console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//    if (err) {
+//      console.log(err);
+//    }
+//    console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
 // LAST ROUTE 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
