0fa297e662c501a797c08d67021ecccb2f4cb201
Chad Smith <chad.smith@vanderbilt.edu>
2022-07-10 23:19:16 -0400

    added voter routes

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
@@ -10,15 +9,19 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-// LAST ROUTE 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
-// Start server on port 3001
-app.listen(PORT, () => {
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+});
